c30b792180376a9766620ef5d8f0b1ce6f3ce9e7
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2023-11-06 21:09:10 +0900

    board crud service controller 추가 create-board.dto.ts 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import {Board, BoardStatus} from "./board.model";
+import {CreateBoardDto} from "./dto/create-board.dto";
 @Controller('boards')
 export class BoardsController {
     constructor(private boardService: BoardsService) {}
+
+
+    @Get('')
+    getAllBoard() : Board[] {
+        return this.boardService.getAllBoards();
+    }
+
+    @Post('')
+    createBoard(@Body() createBoardDto: CreateBoardDto) : Board {
+        return this.boardService.createBoard(createBoardDto);
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: string) : Board {
+        return this.boardService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoardById(@Param() id: string) {
+        return this.boardService.deleteBoardById(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param() id: string,
+        @Body() status: BoardStatus
+    ){
+        return this.boardService.updateBoardStatus(id, status)
+    }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {Board, BoardStatus} from "./board.model";
+import {v1 as uuid} from 'uuid';
+import {CreateBoardDto} from "./dto/create-board.dto";
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    private boards: Board[] = [];
+
+
+    getAllBoards(): Board[] {
+        return this.boards;
+    }
+
+    createBoard(createBoardDto : CreateBoardDto) {
+        const { title, description } = createBoardDto;
+
+        const board : Board = {
+            id : uuid(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC
+        }
+
+        this.boards.push(board)
+        return board
+    }
+
+    getBoardById(id : string) {
+        return this.boards.find((board) => board.id === id);
+    }
+
+    deleteBoardById(id: string) {
+        return this.boards.filter((board) => board.id !== id)
+    }
+
+    updateBoardStatus(id: string, status: BoardStatus) {
+        const board = this.getBoardById(id);
+        board.status = status;
+        return board;
+
+    }
+}
